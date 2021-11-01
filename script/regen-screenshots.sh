ab40d2600be33368f2a8727248dcd2fe42d73a12
lread <lee@dlread.com>
2021-10-31 22:47:12 -0400

    regen-screenshots script image filenames corrected
    They now match up with current filenames used README

--- script/regen-screenshots.sh
+++ script/regen-screenshots.sh
@@ -8,18 +8,18 @@ echo "- msgcat -"
 clj -M:membrane.term screenshot \
     --play doc/examples/play-msgcat.sh \
     --height 14 \
-    --out doc/images/screenshot-play-msgcat.png
+    --out doc/images/screenshot-msgcat.png
 
 echo "- deep-diff -"
 clj -M:membrane.term screenshot \
     --play doc/examples/play-deep-diff.sh \
     --width 80 --height 9 \
     --final-delay 1000 --line-delay 3000 \
-    --out doc/images/screenshot-play-deep-diff.png
+    --out doc/images/screenshot-deep-diff.png
 
 echo "- msgcat with color scheme -"
 clj -M:membrane.term screenshot \
     --play doc/examples/play-msgcat.sh \
     --height 14 \
     --color-scheme "doc/examples/Builtin Solarized Dark.itermcolors" \
-    --out doc/images/screenshot-play-msgcat-scheme.png
+    --out doc/images/screenshot-msgcat-scheme.png
